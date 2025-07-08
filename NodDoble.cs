07b3afe9ffffed125080614a1b15801144f03049
JhersonMes <mishifug@gmail.com>
2025-07-08 02:10:26 -0500

    Add files via upload

---
+++ NodDoble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class NodDoble
+    {
+        public PacienteDoble dato;
+        public NodDoble sig = null;
+        public NodDoble ant = null; 
+    }
+}
